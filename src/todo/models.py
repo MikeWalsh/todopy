24a31717f736421dec8b6fc28fd5c8c2c0cdb928
MikeWalsh <mikewalsh149@gmail.com>
2026-08-02 20:46:41 +0000

    todo add dates

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .db import Base
@@ -8,4 +10,6 @@ class Todo(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(index=True)
     done: Mapped[bool] = mapped_column(default=False)
-    
+    due_date: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
