ec1893d4d1645389d5cf767e293db8212b5f4153
MikeWalsh <mikewalsh149@gmail.com>
2026-08-09 12:08:10 +0000

    basic ui with htmx

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from app.routers import todos
+from app.routers import web
 
 app = FastAPI(title="Todo")
 app.include_router(todos.router)
+app.include_router(web.router)
 
 
